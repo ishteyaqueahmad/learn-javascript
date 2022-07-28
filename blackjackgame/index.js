4217b1cba5190db42c816f64578dd3fa9dee2e69
Ishteyaque <ahmad.ishteyaque966@gmail.com>
2022-07-28 17:29:04 +0530

    blackjack app added

---
+++ blackjackgame/index.js
@@ -0,0 +1,35 @@
+let firstCard= 10
+let secondCard= 10
+
+let sum=firstCard+secondCard
+let hasBlackJack=false
+let isAlive=true
+let message=""
+
+let messageEl=document.getElementById("message-el")
+let sumEl=document.getElementById("sum-el")
+let cardEl=document.getElementById("card-el")
+
+function startGame(){
+
+    sumEl.textContent="Sum:"+sum
+    cardEl.textContent="Cards:"+firstCard+" "+secondCard
+    if(sum<=20){
+        message="Do you want to draw a new card?"
+    }
+    else if(sum===21){
+        message="Wohoo! You've got Blackjack!"
+        hasBlackJack=true
+    }
+    else{
+        message="You're out of game!"
+        isAlive=false
+    }
+    messageEl.textContent=message
+   
+}
+
+function newCard(){
+    console.log("New card added")
+
+}
