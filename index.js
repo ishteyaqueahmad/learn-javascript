690a9cb930d4fd20b93d6f21f88896e66aa5328b
Ishteyaque <ahmad.ishteyaque966@gmail.com>
2022-07-28 13:08:29 +0530

    save button added

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 let count =0
 let countEl=document.getElementById("count-el")
+let saveEl=document.getElementById("save-el")
 function increment(){
     count=count+1
     console.log(count)
     countEl.textContent=count
+}
+
+function save(){
+    let countStr=count+" - "
+    saveEl.textContent+=countStr
+    countEl.textContent=0
+    count=0
+    console.log("save clicked")
 }
