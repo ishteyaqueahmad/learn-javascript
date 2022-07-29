e9d48ba5cb54b9fe53e57c32a2f28d00c0707207
Ishteyaque <ahmad.ishteyaque966@gmail.com>
2022-07-29 16:22:45 +0530

    array concept added for cards

--- blackjackgame/index.js
+++ blackjackgame/index.js
@@ -1,39 +1,48 @@
-let firstCard= 10
-let secondCard= 10
+// 2. Use getRandomCard() to set the values of firstCard and secondCard
+let firstCard = 10
+let secondCard = 4
+let cards = [firstCard, secondCard]
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
-let sum=firstCard+secondCard
-let hasBlackJack=false
-let isAlive=true
-let message=""
+// 1. Create a function, getRandomCard(), that always returns the number 5
 
-let messageEl=document.getElementById("message-el")
-let sumEl=document.getElementById("sum-el")
-let cardEl=document.getElementById("card-el")
 
-function startGame(){
 
-    console.log("startGame is called")
-    sumEl.textContent="Sum:"+sum
-    cardEl.textContent="Cards:"+firstCard+" "+secondCard
-    if(sum<=20){
-        message="Do you want to draw a new card?"
-    }
-    else if(sum===21){
-        message="Wohoo! You've got Blackjack!"
-        hasBlackJack=true
-    }
-    else{
-        message="You're out of game!"
-        isAlive=false
-    }
-    messageEl.textContent=message
-   
+function startGame() {
+    renderGame()
 }
 
-function newCard(){
-    console.log("New card called")
-    let newCard=7
-    sum+=newCard
-    startGame()
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
 
+
+function newCard() {
+    // 3. Use the getRandomCard() to set the value of card
+    let card = 6
+    sum += card
+    cards.push(card)
+    console.log(cards)
+    renderGame()
 }
