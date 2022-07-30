454ed5c6a1df28e40435ee38baaded465cd5bf22
Ishteyaque <ahmad.ishteyaque966@gmail.com>
2022-07-30 20:54:30 +0530

    practice

---
+++ objetandfunctiondemo/index.js
@@ -0,0 +1,11 @@
+let person={
+    name:"Ishteyaque",
+    age:25,
+    country:"india"
+}
+
+function logData(){
+    return person.name+" is "+person.age+" years old and lives in "+person.country
+}
+
+console.log(logData())
